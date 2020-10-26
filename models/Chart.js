5e6ac718f409206a96ec021ce1a71ae991fd37c1
taichan <takecchi.1113@gmail.com>
2020-10-26 11:19:18 +0900

    model chart updated

--- models/Chart.js
+++ models/Chart.js
@@ -1,33 +1,68 @@
 const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
 
-const ChartSchema = mongoose.Schema({
-  TaskID: {
-    type: String,
-    required: true,
+const ChartSchema = new Schema({
+  user: {
+    type: Schema.Types.ObjectId,
+    ref: 'user',
   },
-  TaskName: {
-    type: String,
-    required: true,
-  },
-  Resource: {
-    type: String,
-  },
-  StartDate: {
+
+  chartInfo: [
+    {
+      TaskID: {
+        type: String,
+        required: true,
+      },
+      TaskName: {
+        type: String,
+        required: true,
+      },
+      Resource: {
+        type: String,
+      },
+      StartDate: {
+        type: Date,
+      },
+      EndDate: {
+        type: Date,
+        required: true,
+      },
+      Duration: {
+        type: Number,
+      },
+      PercentComplete: {
+        type: Number,
+        required: true,
+      },
+      Dependencies: {
+        type: String,
+      },
+    },
+  ],
+
+  comments: [
+    {
+      user: {
+        type: Schema.Types.ObjectId,
+        ref: 'user',
+      },
+      text: {
+        type: String,
+        required: true,
+      },
+      name: {
+        type: String,
+      },
+      date: {
+        type: Date,
+        default: Date.now,
+      },
+    },
+  ],
+
+  date: {
     type: Date,
-  },
-  EndDate: {
-    type: Date,
-    required: true,
-  },
-  Duration: {
-    type: Number,
-  },
-  PercentComplete: {
-    type: Number,
-    required: true,
-  },
-  Dependencies: {
-    type: String,
+    default: Date.now,
   },
 });
 
